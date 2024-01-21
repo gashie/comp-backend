4101665cff5a45ab57c576af30044b236a4e01ae
gashie <richardwilliam60@gmail.com>
2024-01-21 22:34:49 +0000

    policy,fleet

--- controllers/fleet/fleet_config.js
+++ controllers/fleet/fleet_config.js
@@ -1,22 +1,22 @@
 const asynHandler = require("../../middleware/async");
 const { sendResponse, CatchHistory } = require("../../helper/utilfunc");
 const GlobalModel = require("../../model/Global");
-const { autoProcessPolicyEnrollment } = require("../../helper/autoCreateEnrollment");
+const { autoProcessServiceToken } = require("../../helper/autoCreateEnrollment");
 const systemDate = new Date().toISOString().slice(0, 19).replace("T", " ");
 
-exports.CreateFleetAgentPolicy = asynHandler(async (req, res, next) => {
+exports.CreateFleetConfigurations = asynHandler(async (req, res, next) => {
     /**
  * Create new role.
- * @param {string} polic_name - Name or title of the application.
- * @param {string} polic_description - Description: Managerial role.
- * @returns {Object} polic_configuration - Object containing role details.
+ * @param {string} fleet_name - Name or title of the application.
+ * @param {string} fleet_description - Description: Managerial role.
+ * @returns {Object} fleet_configuration - Object containing role details.
  */
     let payload = req.body;
-    let results = await GlobalModel.Create(payload, 'policies', '');
-    let { enrollment_token, policy_id } = await autoProcessPolicyEnrollment(req, results.rows)
+    let results = await GlobalModel.Create(payload, 'fleets', '');
+    let { service_token, fleet_id } = await autoProcessServiceToken(req, results.rows)
 
     if (results.rowCount == 1) {
-        return sendResponse(res, 1, 200, "New policy added", { enrollment_token, policy_id, ...req.body })
+        return sendResponse(res, 1, 200, "New fleet added", { service_token, fleet_id, ...req.body })
     } else {
         return sendResponse(res, 0, 200, "Sorry, error saving record: contact administrator", [])
 
@@ -24,10 +24,10 @@ exports.CreateFleetAgentPolicy = asynHandler(async (req, res, next) => {
 
 })
 
-exports.ViewFleetAgentPolicy = asynHandler(async (req, res, next) => {
+exports.ViewFleetConfigurations = asynHandler(async (req, res, next) => {
     // let userData = req.user;
 
-    const tableName = 'policies';
+    const tableName = 'fleets';
     const columnsToSelect = []; // Use string values for column names
     const conditions = [
     ];
@@ -39,39 +39,17 @@ exports.ViewFleetAgentPolicy = asynHandler(async (req, res, next) => {
     sendResponse(res, 1, 200, "Record Found", results.rows)
 })
 
-exports.UpdateFleetAgentPolicy = asynHandler(async (req, res, next) => {
+exports.UpdateFleetConfigurations = asynHandler(async (req, res, next) => {
     let payload = req.body;
     payload.last_updated = systemDate
 
-    const runupdate = await GlobalModel.Update(payload, 'policies', 'policy_id', payload.policy_id)
+    const runupdate = await GlobalModel.Update(payload, 'fleets', 'fleet_id', payload.fleet_id)
     if (runupdate.rowCount == 1) {
         return sendResponse(res, 1, 200, "Record Updated", runupdate.rows[0])
 
 
     } else {
         return sendResponse(res, 0, 200, "Update failed, please try later", [])
-    }
-})
-exports.FindPolicyAndToken = asynHandler(async (req, res, next) => {
-    let { policy_id } = req.body
-    const tableName = 'policies';
-    const columnsToSelect = []; // Use string values for column names
-    const conditions = [
-        { column: 'policy_id', operator: '=', value: policy_id },
-    ];
-    let results = await GlobalModel.Finder(tableName, columnsToSelect, conditions)
-
-    const tableName2 = 'enrollment_tokens';
-    const columnsToSelect2 = []; // Use string values for column names
-    const conditions2 = [
-        { column: 'policy_id', operator: '=', value: policy_id },
-    ];
-    let results2 = await GlobalModel.Finder(tableName2, columnsToSelect2, conditions2)
-    if (results.rowCount == 1) {
-        sendResponse(res, 1, 200, "Record Found", {policy:results.rows[0],etoken:results2.rows[0].token})
-    } else {
-        return sendResponse(res, 0, 200, "Sorry, error saving record: contact administrator", [])
-
     }
 })
 
--- routes/setup.js
+++ routes/setup.js
@@ -5,6 +5,8 @@ const router = express.Router();
 const { CreateSystemRole, ViewSystemRole, UpdateSystemRole, CreateSystemPermission, ViewSystemPermission, UpdateSystemPermission, CreateRolePermission, ViewRolePermission, CreateSystemRoute } = require("../controllers/system/user_management");
 const { VerifyUser, Logout } = require("../controllers/account/auth");
 const { protect } = require("../middleware/auth");
+const { CreateFleetAgentPolicy, ViewFleetAgentPolicy, UpdateFleetAgentPolicy, CreateEnrollmentToken, FindEnrollmentToken, FindPolicyAndToken } = require("../controllers/fleet/policy");
+const { CreateFleetConfigurations, ViewFleetConfigurations, UpdateFleetConfigurations } = require("../controllers/fleet/fleet_config");
 
 
 //routes
@@ -26,11 +28,20 @@ router.route("/system/view_role_permission").post(ViewRolePermission);
 router.route("/system/create_routes").post(CreateSystemRoute);
 
 
-///routes
-// router.route("/create_route").post(protect,CreateSystemRole);
-// router.route("/view_route").post(protect,CreateSystemRole);
-// router.route("/update_route").post(protect,CreateSystemRole);
+///policies and enrollment token
+router.route("/create_policy").post(CreateFleetAgentPolicy);
+router.route("/view_policy").post(ViewFleetAgentPolicy);
+router.route("/find_policy").post(FindPolicyAndToken);
+router.route("/update_policy").post(UpdateFleetAgentPolicy);
 
+router.route("/create_enrollmentoken").post(CreateEnrollmentToken);
+router.route("/view_enrollmentoken").post(FindEnrollmentToken);
+
+
+///fleet and service token
+router.route("/create_fleet").post(CreateFleetConfigurations);
+router.route("/view_fleet").post(ViewFleetConfigurations);
+router.route("/update_fleet").post(UpdateFleetConfigurations);
 
 //user login auth
 router.route("/auth").post(protect, VerifyUser);
